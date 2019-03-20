28be68ef518c60d78156bb4da4978eb269e6a5b1
HeraWer <herawerplay@gmail.com>
2019-03-20 17:05:59 +0100

    pepe

--- src/PracticaGrup/Autobus.java
+++ src/PracticaGrup/Autobus.java
@@ -1,9 +1,8 @@
 package PracticaGrup;
 
-public interface Autobus {
-
-	public static void hola() {
-		System.out.println("HHHHHHHHHHHHHHHHHHHHHH");
-		System.out.println("jiwdjefowjfofrndrflreehk");
-	}
+public class Autobus {
+	
+	private int id;
+	
+	
 }
