492e1be988fc850b6e098698fab0c41ecc35aa47
Joan <Joan@DESKTOP-6315C8G>
2019-03-20 17:19:20 +0100

    push&commit

--- src/PracticaGrup/Autobus.java
+++ src/PracticaGrup/Autobus.java
@@ -1,17 +1,43 @@
 package PracticaGrup;
 
-public class Autobus {
+public class Autobus{
 	
 	private int idNum;
 	private Conductor conductor;
 	private double preuBase;
 	
 	public Autobus(int idNum, Conductor conductor, double preuBase) {
-		super();
 		this.idNum = idNum;
 		this.conductor = conductor;
 		this.preuBase = preuBase;
 	}
+
+	public int getIdNum() {
+		return idNum;
+	}
+
+	public void setIdNum(int idNum) {
+		this.idNum = idNum;
+	}
+
+	public Conductor getConductor() {
+		return conductor;
+	}
+
+	public void setConductor(Conductor conductor) {
+		this.conductor = conductor;
+	}
+
+	public double getPreuBase() {
+		return preuBase;
+	}
+
+	public void setPreuBase(double preuBase) {
+		this.preuBase = preuBase;
+	}
 	
+	public double calculaPreu() {
+		return 2.3;
+	}
 	
 }
